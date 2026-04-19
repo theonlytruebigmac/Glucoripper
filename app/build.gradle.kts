912c8210917aa8c4fa715a24ff6cdb44376db765
theonlytruebigmac <znfrazier@gmail.com>
2026-04-19 15:38:17 -0400

    feat: Bump version codes to 0.4.2 for app and wear modules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.syschimp.glucoripper"
         minSdk = 31
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.4.1"
+        versionCode = 8
+        versionName = "0.4.2"
     }
 
     signingConfigs {
